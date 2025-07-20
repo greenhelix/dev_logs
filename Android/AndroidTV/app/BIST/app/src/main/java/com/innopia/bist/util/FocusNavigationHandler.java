e6362aa213aebe26f97d9de2087509aaf3704985
GreenHelix <49743467+greenhelix@users.noreply.github.com>
2025-07-20 16:59:06 +0900

    BIST update
    BIST update

--- Android/AndroidTV/app/BIST/app/src/main/java/com/innopia/bist/util/FocusNavigationHandler.java
+++
@@ -1,5 +0,0 @@
-package com.innopia.bist.util;
-
-public interface FocusNavigationHandler {
-    int getTargetFocusId(int direction);
-}
--- Android/AndroidTV/app/BIST/app/src/main/java/com/innopia/bist/util/ILogger.java
+++
@@ -1,6 +0,0 @@
-package com.innopia.bist.util;
-
-public interface ILogger {
-    void log(String tag, String message);
-    void log(String message);
-}
